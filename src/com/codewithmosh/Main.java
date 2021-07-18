d9146112e36e512b5c5c24e12e317ecff29fdc7e
damercadoo <damercadoo@unal.edu.co>
2021-07-18 10:35:06 -0500

    boxing and unboxing

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -16,9 +16,9 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-        var list=new GenericList<User>();
-        list.add(new User());
-        User number=list.get(0);
+       GenericList<Integer> numbers=new GenericList<>();
+       numbers.add(1);
+       int number=numbers.get(0);
     }
 
 }
