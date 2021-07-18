806e23350d944768e9b4c62d073450c32fd1e770
damercadoo <damercadoo@unal.edu.co>
2021-07-18 08:40:56 -0500

    catch exceptions

--- src/com/codewithmosh/exceptions/ExceptionsDemo.java
+++ src/com/codewithmosh/exceptions/ExceptionsDemo.java
@@ -1,11 +1,26 @@
 package com.codewithmosh.exceptions;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class ExceptionsDemo {
     public static void show(){
-        sayHello(null);
+        try {
+            var reader=new FileReader("file.txt");
+            var value=reader.read();
+            new SimpleDateFormat().parse("");
+        } catch (FileNotFoundException e) {
+            System.out.println("file does not exist");
+        } catch (IOException e) {
+            System.out.println("could not read data");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    public static void sayHello(String name){
-        System.out.println(name.toUpperCase());
-    }
+
 }
