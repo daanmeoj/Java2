6b194e30f5228f2da33bcf166feae8535017edc2
damercadoo <damercadoo@unal.edu.co>
2021-07-18 08:57:34 -0500

    try with resources

--- src/com/codewithmosh/exceptions/ExceptionsDemo.java
+++ src/com/codewithmosh/exceptions/ExceptionsDemo.java
@@ -2,30 +2,21 @@ package com.codewithmosh.exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show(){
-        FileReader reader=null;
-        try {
-            reader=new FileReader("file.txt");
+        try(
+                var reader=new FileReader("file.txt");
+                var writer=new FileWriter("some file");
+            ) {
             var value=reader.read();
         } catch (IOException e) {
             System.out.println("could not read data");
         }
-        finally {
-            if(reader!=null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-
     }
 
 
