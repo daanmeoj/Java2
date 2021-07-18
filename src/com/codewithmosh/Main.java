f17029debd554426824a0cac1ae6108a81f1aca9
damercadoo <damercadoo@unal.edu.co>
2021-07-18 09:14:11 -0500

    rethrowing exceptions

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -4,10 +4,16 @@ import com.codewithmosh.TextBox;
 import com.codewithmosh.UIControl;
 import com.codewithmosh.exceptions.ExceptionsDemo;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        ExceptionsDemo.show();
+        try {
+            ExceptionsDemo.show();
+        } catch (Throwable e) {
+            System.out.println("unexpected error occurred");
+        }
     }
 
 }
